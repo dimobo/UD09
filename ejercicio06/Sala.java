6ccec04835479884d493f9a6c86f7a24627a9d0b
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-07-21 22:10:53 +0200

    06 update

--- ejercicio06/Sala.java
+++ ejercicio06/Sala.java
@@ -1,5 +1,22 @@
 package ejercicio06;
 
+import java.util.Random;
+
 public class Sala {
+
+	public String[][] sentarPersona(String[][] asiento) {
+		Random r = new Random();
+
+		while (true) {
+			int r1 = r.nextInt((8 - 0) + 1) + 0;
+			int r2 = r.nextInt((9 - 0) + 1) + 0;
+
+			if (asiento[r1][r2].equals("")) {
+				asiento[r1][r2] = "~";
+				return asiento;
+			}
+
+		}
+	}
 
 }
--- ejercicio06/ejercicio06APP.java
+++ ejercicio06/ejercicio06APP.java
@@ -1,5 +1,9 @@
 package ejercicio06;
 
 public class ejercicio06APP {
+	
+	public static void main(String[] args) {
+
+	}
 
 }
