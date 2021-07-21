3f6e8f3d90bd4d556a385392f6300746ee7f2bcc
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-07-21 23:01:30 +0200

    06 Sala

--- ejercicio06/Sala.java
+++ ejercicio06/Sala.java
@@ -1,22 +1,70 @@
 package ejercicio06;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Sala {
 
-	public String[][] sentarPersona(String[][] asiento) {
+	// Atributo de sala
+	private String[][] asiento = new String[8][9];
+
+	// Constructor de clase
+	public Sala() {
+		asiento = iniciarSala();
+	}
+
+	// Iniciar sala
+	public String[][] iniciarSala() {
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 9; j++) {
+				asiento[i][j] = "X";
+			}
+		}
+
+		return asiento;
+
+	}
+
+	// Metodo para sentar a una persona.
+	public String[][] sentarPersona() {
 		Random r = new Random();
 
 		while (true) {
 			int r1 = r.nextInt((8 - 0) + 1) + 0;
 			int r2 = r.nextInt((9 - 0) + 1) + 0;
-
-			if (asiento[r1][r2].equals("")) {
+			if (asiento[r1][r2].equals("X")) {
 				asiento[r1][r2] = "~";
 				return asiento;
 			}
-
 		}
+	}
+
+	// Leer sala.
+	public void leerSala() {
+		for (int i = 7; i >= 0; i--) {
+			for (int j = 0; j <= 8; j++) {
+
+				System.out.print(Integer.toString(i + 1) + listaLetras().get(j) + asiento[i][j] + " ");
+
+			}
+			System.out.print("\n");
+		}
+	}
+
+	// Lista de la sala
+	private ArrayList<Character> listaLetras() {
+		ArrayList<Character> lista = new ArrayList<Character>();
+		lista.add('A');
+		lista.add('B');
+		lista.add('C');
+		lista.add('D');
+		lista.add('E');
+		lista.add('F');
+		lista.add('G');
+		lista.add('H');
+		lista.add('I');
+		return lista;
 	}
 
 }
--- ejercicio06/ejercicio06APP.java
+++ ejercicio06/ejercicio06APP.java
@@ -3,7 +3,12 @@ package ejercicio06;
 public class ejercicio06APP {
 	
 	public static void main(String[] args) {
+		
+		Sala s1 = new Sala();
 
+		
+		s1.leerSala();
+		
 	}
 
 }
