7ec584d8f1ed295ce854c8a8d3ab57b211c8a5f1
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-07-21 19:10:53 +0200

    03 ok

--- ejercicio03/ejercicio03APP.java
+++ ejercicio03/ejercicio03APP.java
@@ -2,4 +2,19 @@ package ejercicio03;
 
 public class ejercicio03APP {
 
+	public static void main(String[] args) {
+
+		Libro uno = new Libro("8-750-987", "La brujula dorada", "Philip Pullman", 360),
+				dos = new Libro("9-784-587", "La Morada", "R.A Salvatore", 400);
+
+		System.out.println(uno.toString());
+		System.out.println(dos.toString());
+
+		if (uno.getNumPaginas() > dos.getNumPaginas()) {
+			System.out.println(uno.getTitulo() + " tiene mas paginas.");
+		} else {
+			System.out.println(dos.getTitulo() + " tiene mas paginas.");
+		}
+	}
+
 }
